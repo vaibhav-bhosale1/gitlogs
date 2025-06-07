6bc0134862866608689d9e64c4e99149de11563b
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2025-06-07 22:52:33 +0530

    git log

--- my.py
+++ my.py
@@ -1,9 +1,13 @@
 
 print("hello from saibasjoj av")
 print("division")
+<<<<<<< HEAD
 
 print("rebasemerge")
 
 
 print("test commit")
+=======
+print("merge")
+>>>>>>> c5b9aae (fourth merge commit)
 
